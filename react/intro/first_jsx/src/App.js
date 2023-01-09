7ff013cd8003536a694c7c2f1427a74a52b8abd4
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-08 18:40:57 -0800

    Create First JSX assignment

---
+++ react/intro/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;

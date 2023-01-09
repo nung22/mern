927c6d9347290c0ba8d6fd59c0d0874c1927a5d3
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-09 10:20:02 -0800

    Finish Putting It Together assignment

--- react/class_components/prop_it_up/src/App.js
+++ react/class_components/prop_it_up/src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="App">
       <div className="Person-Card-list">
-        <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"}/>
-        <PersonCard firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"}/>
-        <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"}/>
-        <PersonCard firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"}/>
+        <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+        <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+        <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+        <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
       </div>
     </div>
   );

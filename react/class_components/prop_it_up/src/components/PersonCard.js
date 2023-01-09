c4b3d71c39bc3e1aacfc3d555df8f736ac91f443
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-09 13:36:55 -0800

    Update PersonCard.js

--- react/class_components/prop_it_up/src/components/PersonCard.js
+++ react/class_components/prop_it_up/src/components/PersonCard.js
@@ -13,7 +13,7 @@ class PersonCard extends Component {
         <h2>{this.props.lastName}, {this.props.firstName}</h2>
         <p>Age: {this.state.age}</p>
         <p>Hair Color: {this.props.hairColor}</p>
-        <button class="btn btn-secondary" onClick={() => this.setState({ age: this.state.age+1 })}>
+        <button className="btn btn-secondary" onClick={() => this.setState({ age: this.state.age+1 })}>
           Birthday Button for {this.props.firstName} {this.props.lastName}
           </button>
       </div>

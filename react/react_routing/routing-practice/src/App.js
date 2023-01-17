1b8239a47e68c7b8c5d38f3536f61e6a4ec7fd17
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-16 23:37:24 -0800

    Finish Routing Practice assignment

--- react/react_routing/routing-practice/src/App.js
+++ react/react_routing/routing-practice/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { useParams } from "react-router";
+import Survey from "./components/Survey";
+
+const Home = (props) => {
+  return (
+    <h1>Welcome</h1>
+  );
+}
+
+const DisplayInput = (props) => {
+  const {input} = useParams();
+
+  return (
+    <h1>
+      {
+        isNaN(input)
+        ? 'The word is: '
+        : 'The number is: '
+      }
+      {input}
+    </h1>
+  );
+}
+
+const DisplayMessage = (props) => {
+  const {word, textColor, backgroundColor} = useParams(); 
+
+  return ( 
+    <h1 className='p-5' style={{color:`${textColor}`, backgroundColor:`${backgroundColor}`}}>The word is: {word}</h1>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center p-8 text-4xl'>
+      <Routes>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/:input' element={<DisplayInput />}/>
+        <Route path='/:word/:textColor/:backgroundColor' element={<DisplayMessage />}/>
+      </Routes>
     </div>
   );
 }
+
 
 export default App;

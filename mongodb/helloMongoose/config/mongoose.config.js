f21792507054cb05fc543e784e322e87129014d0
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-18 18:42:54 -0800

    Update mongoose.config.js

--- mongodb/helloMongoose/config/mongoose.config.js
+++ mongodb/helloMongoose/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/hello_mongoose', {
   useNewUrlParser: true,
-  ="function call from-rainbow">useUnifiedTopology: true
+  useUnifiedTopology: true
 })
   .then(() => console.log('Established a conneciton to the database'))
   .catch(err => console.log('Something went wrong when connecting to the database ', err));
